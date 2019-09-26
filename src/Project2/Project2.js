9a5ca7da9c82156db401c6c647f1272582654ad0
jsissjoo <wpdldpa1003@gmail.com>
2019-09-26 13:38:02 +0900

    Done project2

--- src/Project2/Project2.js
+++ src/Project2/Project2.js
@@ -7,59 +7,65 @@ class Project2 extends React.Component{
     return(
       <div className="project2__container">
         <header>
+          <div className="header__status-bar">
+            <div className="header__status-bar-left">
+              <span>18:00</span>
+            </div>
+            <div className="header__status-bar-right">
+              <span className="bar__icon">D-</span>
+              <span className="bar__icon">W-</span>
+              <span className="bar__icon">B-</span>
+            </div>
+          </div>
           <h1>Add meals</h1>
         </header>
         <nav className="project2__nav">
           <ul className="navigation__list">
             <li className="navigation__item">
-              <span className="item__icon">🥪</span>
+              <span className="item__icon" role="img" >🥪</span>
               <span className="itme__text">All</span>
             </li>
             <li className="navigation__item">
-              <span className="item__icon">🍕</span>
+              <span className="item__icon" role="img">🍕</span>
               <span className="itme__text">Pizza</span>
             </li>
             <li className="navigation__item">
-              <span className="item__icon">🥩</span>
+              <span className="item__icon" role="img">🥩</span>
               <span className="itme__text">Meat</span>
             </li>
             <li className="navigation__item">
-              <span className="item__icon">🥗</span>
+              <span className="item__icon" role="img">🥗</span>
               <span className="itme__text">Veggies</span>
             </li>
           </ul>
         </nav>
         <section className="featured-meals__sec">
           <h2 className="featured-meals__title">Featured meals</h2>
-            <div className="featured-meal__wrapper">
+          <div className="featured-meals__menu">
+            <div className="featured-meal__wrapper wrapper1">
               <article className="featured-meal">
                 <h4 className="featured-meal__title">Penne Broccoli</h4>
-                <button className="featured-meal__btn"><span>➕</span></button>
+                <button className="featured-meal__btn"><span role="img">➕</span></button>
               </article>
             </div>
-            <div className="featured-meal__wrapper">
+            <div className="featured-meal__wrapper wrapper2">
               <article className="featured-meal">
                 <h4 className="featured-meal__title">Penne Broccoli</h4>
-                <button className="featured-meal__btn"><span>➕</span></button>
+                <button className="featured-meal__btn"><span role="img">➕</span></button>
               </article>
             </div>
-            <div className="featured-meal__wrapper">
+            <div className="featured-meal__wrapper wrapper3">
               <article className="featured-meal">
                 <h4 className="featured-meal__title">Penne Broccoli</h4>
-                <button className="featured-meal__btn"><span>➕</span></button>
+                <button className="featured-meal__btn"><span role="img">➕</span></button>
               </article>
             </div>
-            <div className="featured-meal__wrapper">
+            <div className="featured-meal__wrapper wrapper4">
               <article className="featured-meal">
                 <h4 className="featured-meal__title">Penne Broccoli</h4>
-                <button className="featured-meal__btn"><span>➕</span></button>
+                <button className="featured-meal__btn"><span role="img">➕</span></button>
               </article>
             </div>
-            <div className="featured-meal__wrapper">
-              <article className="featured-meal">
-                <h4 className="featured-meal__title">Penne Broccoli</h4>
-                <button className="featured-meal__btn"><span>➕</span></button>
-              </article>
             </div>
         </section>
 
@@ -68,21 +74,28 @@ class Project2 extends React.Component{
             <li className="recipe"><span className="recipe__name">Pasta Carbonara With ham</span><button className="recipe__btn">➕</button></li>
             <li className="recipe"><span className="recipe__name">Lasagna Bolognese</span><button className="recipe__btn">➕</button></li>
             <li className="recipe"><span className="recipe__name">Spoghetti Bowl with Chicken</span><button className="recipe__btn">➕</button></li>
+            <li className="recipe"><span className="recipe__name">Pasta Carbonara With ham</span><button className="recipe__btn">➕</button></li>
+            <li className="recipe"><span className="recipe__name">Lasagna Bolognese</span><button className="recipe__btn">➕</button></li>
+            <li className="recipe"><span className="recipe__name">Spoghetti Bowl with Chicken</span><button className="recipe__btn">➕</button></li>
+            <li className="recipe"><span className="recipe__name">Pasta Carbonara With ham</span><button className="recipe__btn">➕</button></li>
+            <li className="recipe"><span className="recipe__name">Lasagna Bolognese</span><button className="recipe__btn">➕</button></li>
+            <li className="recipe"><span className="recipe__name">Spoghetti Bowl with Chicken</span><button className="recipe__btn">➕</button></li>
+          
           </ul>
         </section>
 
         <footer>
           <ul className="footer__list">
             <li className="footer__item">
-              <img className=""></img>
+              <button className="footer__item-btn__selected">🍳</button>
               <span>Recipies</span>
             </li>
             <li className="footer__item">
-              <img className=""></img>
+              <button className="footer__item-btn">🍱</button>
               <span>Meals</span>
             </li>
             <li className="footer__item">
-              <img className=""></img>
+              <button className="footer__item-btn">🛒</button>
               <span>Shopping</span>
             </li>
           </ul>
---
+++ src/Project3/Project3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./project3.scss";
+
+class Project3 extends React.Component{
+  render(){
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default Project3;
